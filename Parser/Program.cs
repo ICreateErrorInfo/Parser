5dc74db8a9fd45c39a46e8256a8ff9023ad59070
ICreateErrorInfo <hm.Github@smjh.de>
2021-10-16 20:46:06 +0200

    Add project files.

---
+++ Parser/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Parser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser parser = new Parser();
+
+            string program = "\"Hello\"";
+
+            ASTNode ast = parser.Parse(program);
+
+            Console.WriteLine(ast.ToString());
+        }
+    }
+}
---
+++ Parser/Tokenizer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Parser
+{
+    public class Tokenizer
+    {
+        string _string;
+        int _cursor;
+
+        public void init(string s)
+        {
+            _string = s;
+            _cursor = 0;
+        }
+
+        private bool hasMoreTokens()
+        {
+            return _cursor < _string.Length;
+        }
+
+        public ASTNode getNextToken()
+        {
+            if (!hasMoreTokens())
+            {
+                return null;
+            }
+
+            char[] str = _string.ToCharArray();
+
+            MatchCollection matched = Regex.Matches(_string, @"^\d+");
+            if(matched != null && matched.Count != 0)
+            {
+                _cursor += matched.Count;
+                return new ASTNode(
+                    type: "NUMBER",
+                    value: matched[0].ToString()
+                );
+            }
+
+            matched = Regex.Matches(_string, "^\"[^\"]*\"");
+            if (matched != null && matched.Count != 0)
+            {
+                _cursor += matched.Count;
+                return new ASTNode(
+                    type: "STRING",
+                    value: matched[0].ToString()
+                );
+            }
+
+            matched = Regex.Matches(_string, "'[^']*'");
+            if (matched != null && matched.Count != 0)
+            {
+                _cursor += matched.Count;
+                return new ASTNode(
+                    type: "STRING",
+                    value: matched[0].ToString()
+                );
+            }
+
+            return null;
+        }
+    }
+}
